ed61d55e00edfffbfc6a7c78effd10637ee022ba
Ivan Colomer <ivancolomer.1@gmail.com>
2019-06-07 14:27:10 +0200

    Removing deprecated Google Drive API and switching to Google Drive REST v3

--- android-app/app/src/main/java/com/lglab/ivan/lgxeducontroller/activities/GoogleDriveActivity.java
+++ android-app/app/src/main/java/com/lglab/ivan/lgxeducontroller/activities/GoogleDriveActivity.java
@@ -2,6 +2,7 @@ package com.lglab.ivan.lgxeducontroller.activities;
 
 import android.content.Intent;
 import android.content.IntentSender;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,72 +11,71 @@ import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveContents;
-import com.google.android.gms.drive.DriveFile;
-import com.google.android.gms.drive.DriveFolder;
-import com.google.android.gms.drive.DriveId;
-import com.google.android.gms.drive.MetadataChangeSet;
-import com.google.android.gms.drive.OpenFileActivityOptions;
-import com.google.android.gms.drive.query.Filters;
-import com.google.android.gms.drive.query.SearchableField;
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
+import com.lglab.ivan.lgxeducontroller.drive.DriveServiceHelper;
 import com.lglab.ivan.lgxeducontroller.drive.GoogleDriveManager;
 import com.lglab.ivan.lgxeducontroller.games.quiz.Quiz;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static com.google.android.gms.drive.Drive.getDriveResourceClient;
-import static com.lglab.ivan.lgxeducontroller.drive.GoogleDriveManager.RC_SIGN_IN;
+import java.util.Collections;
 
 public abstract class GoogleDriveActivity extends AppCompatActivity {
 
-    private TaskCompletionSource<DriveId> mOpenItemTaskSource;
-
     @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         switch (requestCode) {
-            case RC_SIGN_IN:
-                if (resultCode == RESULT_OK) {
-                    Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
-                    handleSignInResult(GoogleSignIn.getSignedInAccountFromIntent(data));
+            case GoogleDriveManager.RC_SIGN_IN:
+                if (resultCode == RESULT_OK && resultData != null) {
+                    handleSignInResult(resultData);
                     return;
                 }
 
-                Log.e(GoogleDriveManager.TAG, "Sign-in failed with resultCode = " + String.valueOf(resultCode));
-//                finish();
+                Log.e(GoogleDriveManager.TAG, "Sign-in failed with resultCode = " + resultCode);
+                showMessage("Unable to sign in. Make sure you have internet connection and try again.");
+                //finish();
                 break;
-            case GoogleDriveManager.RC_OPEN_ITEM:
-                if (resultCode == RESULT_OK) {
-                    mOpenItemTaskSource.setResult(data.getParcelableExtra(
-                            OpenFileActivityOptions.EXTRA_RESPONSE_DRIVE_ID));
-                } else {
-                    //mOpenItemTaskSource.setException(new RuntimeException("Unable to open file"));
+            case GoogleDriveManager.RC_OPEN_FILE:
+                if (resultCode == RESULT_OK && resultData != null) {
+                    Uri uri = resultData.getData();
+                    if (uri != null) {
+                        openFileFromFilePicker(uri);
+                    }
+                }
+                break;
+            case GoogleDriveManager.RC_OPEN_FOLDER:
+                if (resultCode == RESULT_OK && resultData != null) {
+                    Uri uri = resultData.getData();
+                    if (uri != null) {
+                        openFolderFromFolderPicker(uri);
+                    }
                 }
                 break;
         }
+
+        super.onActivityResult(requestCode, resultCode, resultData);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (GoogleDriveManager.GoogleSignInClient != null && GoogleDriveManager.DriveClient != null && GoogleDriveManager.DriveResourceClient != null)
+        signIn();
+    }
+
+    private void signIn() {
+        if (GoogleDriveManager.GoogleSignInClient != null && GoogleDriveManager.DriveServiceHelper != null)
             return;
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
-                .requestScopes(Drive.SCOPE_FILE)
+                .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                //The scope should be changed in order to see other files... https://developers.google.com/drive/api/v3/about-auth https://www.googleapis.com/auth/drive
                 .build();
 
         GoogleDriveManager.GoogleSignInClient = GoogleSignIn.getClient(this, gso);
@@ -85,12 +85,124 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
             GoogleDriveManager.GoogleSignInClient.signOut();
 
         Intent signInIntent = GoogleDriveManager.GoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        startActivityForResult(signInIntent, GoogleDriveManager.RC_SIGN_IN);
     }
+
+    public void handleSignInResult(Intent result) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+                .addOnSuccessListener(googleAccount -> {
+                    Log.d(GoogleDriveManager.TAG, "Signed in as " + googleAccount.getEmail());
+                    showMessage("Signed in as " + googleAccount.getEmail());
+
+                    // Use the authenticated account to sign in to the Drive service.
+                    GoogleAccountCredential credential =
+                            GoogleAccountCredential.usingOAuth2(
+                                    this, Collections.singleton(DriveScopes.DRIVE_FILE)); //SCOPE_FILE
+                    credential.setSelectedAccount(googleAccount.getAccount());
+                    Drive googleDriveService = new Drive.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credential)
+                            .setApplicationName("LGxEDU")
+                            .build();
+
+                    GoogleDriveManager.DriveServiceHelper = new DriveServiceHelper(googleDriveService);
+
+                    openFilePicker();
+                })
+                .addOnFailureListener(exception -> Log.e(GoogleDriveManager.TAG, "Unable to sign in.", exception));
+    }
+
+
+
+    /**
+     * Opens the Storage Access Framework file picker using GoogleDriveManager.RC_OPEN_FILE.
+     */
+    private void openFilePicker() {
+        if (GoogleDriveManager.DriveServiceHelper != null) {
+            Log.d(GoogleDriveManager.TAG, "Opening file picker.");
+
+            Intent pickerIntent = GoogleDriveManager.DriveServiceHelper.createFilePickerIntent();
+
+            // The result of the SAF Intent is handled in onActivityResult.
+            startActivityForResult(pickerIntent, GoogleDriveManager.RC_OPEN_FILE);
+        }
+    }
+
+    /**
+     * Opens the Storage Access Framework file picker using GoogleDriveManager.RC_OPEN_FOLDER.
+     */
+    private void openFolderPicker() {
+        if (GoogleDriveManager.DriveServiceHelper != null) {
+            Log.d(GoogleDriveManager.TAG, "Opening folder picker.");
+
+            Intent pickerIntent = GoogleDriveManager.DriveServiceHelper.createFolderPickerIntent();
+
+            // The result of the SAF Intent is handled in onActivityResult.
+            startActivityForResult(pickerIntent, GoogleDriveManager.RC_OPEN_FOLDER);
+        }
+    }
+
+
+    /**
+     * Opens a file from its {@code uri} returned from the Storage Access Framework file picker
+     * initiated by {@link #openFilePicker()}.
+     */
+    private void openFileFromFilePicker(Uri uri) {
+        if (GoogleDriveManager.DriveServiceHelper != null) {
+            Log.d(GoogleDriveManager.TAG, "Opening " + uri.getPath());
+
+            GoogleDriveManager.DriveServiceHelper.openFileUsingStorageAccessFramework(getContentResolver(), uri)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        // Files opened through SAF cannot be modified.
+                        GoogleDriveManager.setReadOnlyMode(name, content);
+                        Log.d(GoogleDriveManager.TAG, name + ": " + content);
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e(GoogleDriveManager.TAG, "Unable to open file from picker.", exception));
+        }
+    }
+
+    /**
+     * Opens a file from its {@code uri} returned from the Storage Access Framework file picker
+     * initiated by {@link #openFilePicker()}.
+     */
+    private void openFolderFromFolderPicker(Uri uri) {
+        if (GoogleDriveManager.DriveServiceHelper != null) {
+            Log.d(GoogleDriveManager.TAG, "Opening " + uri.getPath());
+
+            GoogleDriveManager.DriveServiceHelper.openFileUsingStorageAccessFramework(getContentResolver(), uri)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        // Files opened through SAF cannot be modified.
+                        GoogleDriveManager.setReadOnlyMode(name, content);
+                        Log.d(GoogleDriveManager.TAG, name + ": " + content);
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e(GoogleDriveManager.TAG, "Unable to open file from picker.", exception));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void importQuiz() {
         Toast.makeText(this, "importQuiz", Toast.LENGTH_SHORT).show();
-        if (GoogleDriveManager.DriveClient != null && GoogleDriveManager.DriveResourceClient != null) {
+        /*if (GoogleDriveManager.DriveClient != null && GoogleDriveManager.DriveResourceClient != null) {
             pickTextFile()
                     .addOnSuccessListener(this,
                             driveId -> handleReadItem(driveId.asDriveFile()))
@@ -98,11 +210,11 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
                         Log.e(GoogleDriveManager.TAG, "No file selected", e);
                         finish();
                     });
-        }
+        }*/
     }
 
     public void exportQuiz(Quiz quiz) {
-        if (GoogleDriveManager.DriveClient != null && GoogleDriveManager.DriveResourceClient != null) {
+        /*if (GoogleDriveManager.DriveClient != null && GoogleDriveManager.DriveResourceClient != null) {
             pickFolder()
                     .addOnSuccessListener(this,
                             driveId -> handleSaveItem(driveId.asDriveFolder(), quiz))
@@ -110,10 +222,10 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
                         Log.e(GoogleDriveManager.TAG, "No folder selected", e);
                         finish();
                     });
-        }
+        }*/
     }
 
-    private Task<DriveId> pickTextFile() {
+    /*private Task<DriveId> pickTextFile() {
         OpenFileActivityOptions openOptions =
                 new OpenFileActivityOptions.Builder()
                         //.setSelectionFilter(Filters.eq(SearchableField.MIME_TYPE, "text/plain"))
@@ -143,23 +255,11 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
                     return null;
                 });
         return mOpenItemTaskSource.getTask();
-    }
+    }*/
 
-    public void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            GoogleDriveManager.DriveClient = Drive.getDriveClient(this, account);
-            GoogleDriveManager.DriveResourceClient = getDriveResourceClient(this, account);
-            showMessage("Logged into " + account.getEmail());
 
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(GoogleDriveManager.TAG, "signInResult:failed code=" + e.getStatusCode());
-        }
-    }
 
-    public void handleReadItem(final DriveFile file) {
+    /*public void handleReadItem(final DriveFile file) {
         try {
             GoogleDriveManager.DriveResourceClient.openFile(file, DriveFile.MODE_READ_ONLY)
                     .continueWithTask(task -> {
@@ -180,17 +280,15 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
                         Task<Void> discardTask = GoogleDriveManager.DriveResourceClient.discardContents(contents);
                         return discardTask;
                     })
-                    /*.addOnSuccessListener(this,
-                            driveFile -> showMessage("File imported successfully"))*/
                     .addOnFailureListener(e -> {
                         Log.e(GoogleDriveManager.TAG, e.toString());
                     });
         } catch (Exception e) {
             Log.e(GoogleDriveManager.TAG, e.toString());
         }
-    }
+    }*/
 
-    private void handleSaveItem(final DriveFolder parent, Quiz quiz) {
+    /*private void handleSaveItem(final DriveFolder parent, Quiz quiz) {
         GoogleDriveManager.DriveResourceClient
                 .createContents()
                 .continueWithTask(task -> {
@@ -202,7 +300,6 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
 
                     MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
                             .setTitle(quiz.getNameForExporting())
-                            //.setMimeType("text/plain")
                             .setMimeType("application/json")
                             .setStarred(true)
                             .build();
@@ -215,8 +312,7 @@ public abstract class GoogleDriveActivity extends AppCompatActivity {
                     Log.e(GoogleDriveManager.TAG, "Unable to create file", e);
                     showMessage("Unable to create file");
                 });
-    }
-
+    }*/
 
     public void showMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
--- android-app/app/src/main/java/com/lglab/ivan/lgxeducontroller/drive/GoogleDriveManager.java
+++ android-app/app/src/main/java/com/lglab/ivan/lgxeducontroller/drive/GoogleDriveManager.java
@@ -1,19 +1,32 @@
 package com.lglab.ivan.lgxeducontroller.drive;
 
-
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.drive.DriveClient;
-import com.google.android.gms.drive.DriveResourceClient;
 
 public class GoogleDriveManager {
 
     public static final String TAG = "drive_log_in";
 
     public static final int RC_SIGN_IN = 0;
-    public static final int RC_OPEN_ITEM = 1;
+    public static final int RC_OPEN_FILE = 1;
+    public static final int RC_OPEN_FOLDER = 2;
 
     public static GoogleSignInClient GoogleSignInClient;
-    public static DriveClient DriveClient;
-    public static DriveResourceClient DriveResourceClient;
+    public static DriveServiceHelper DriveServiceHelper;
+
+    private static String OpenFileId;
+    private static String OpenFileName;
+    private static String OpenFileContent;
+
+    public static void setReadOnlyMode(String fileName, String fileContent) {
+        OpenFileId = null;
+        OpenFileName = fileName;
+        OpenFileContent = fileContent;
+    }
+
+    public static void setReadWriteMode(String fileId, String fileName, String fileContent) {
+        OpenFileId = fileId;
+        OpenFileName = fileName;
+        OpenFileContent = fileContent;
+    }
 
 }

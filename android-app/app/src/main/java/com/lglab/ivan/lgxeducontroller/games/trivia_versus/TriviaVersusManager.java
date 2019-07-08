58c6ca7fd08b7cbb1af7db0f2f2acecb7259a8b0
Ivan Colomer <ivancolomer.1@gmail.com>
2019-07-08 16:29:05 +0200

    adding classes to triviaversusmanager

--- android-app/app/src/main/java/com/lglab/ivan/lgxeducontroller/games/trivia_versus/TriviaVersusManager.java
+++ android-app/app/src/main/java/com/lglab/ivan/lgxeducontroller/games/trivia_versus/TriviaVersusManager.java
@@ -9,7 +9,18 @@ public class TriviaVersusManager extends TriviaManager {
 
     private List<Integer> selectedAnswers;
 
+    private Player[] players;
+
     public TriviaVersusManager(Game game) {
         super(game);
+    }
+
+
+    private static class Player {
+        private String name;
+
+        Player(String name) {
+            this.name = name;
+        }
     }
 }
